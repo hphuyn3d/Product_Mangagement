3e3a350b17e775b0d0cf9b690023b54e4063811c
hhuynh <hhuynh@connellypartners.com>
2018-01-17 23:04:14 -0500

    Hooking in the filter functionality

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -13,7 +13,20 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    errorMessage: string;
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string){
+        this._listFilter = value;
+        // if there is a filter list value then performFilter. If empty then set to entire list of products
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             'productId': 1,
@@ -66,6 +79,17 @@ export class ProductListComponent implements OnInit {
             'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
         }
     ];
+
+    // Function that is executed when the component is first initialized
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'rake';
+    }
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+              product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
      toggleImage(): void {
         this.showImage = !this.showImage;
     }
