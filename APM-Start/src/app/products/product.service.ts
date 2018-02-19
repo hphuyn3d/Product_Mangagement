c28ef8c41e93ed4e8be8192589611761b14155cf
hhuynh <hhuynh@connellypartners.com>
2018-02-18 22:24:22 -0500

    Created a service, has a method that will return an Array

---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
+
+@Injectable()
+export class ProductService {
+    getProducts(): IProduct[] {
+        return;
+    }
+}
