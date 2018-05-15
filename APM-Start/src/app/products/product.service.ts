0968473918bb9e017f2604daa6f071cbf413210b
hhuynh <hhuynh@connellypartners.com>
2018-05-14 23:27:25 -0400

    imported HttpClient and Observable, Created a constructor, specified the parameter _http so angular will then inject the HttpClient service instance into the variable

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import { IProduct } from './product';
 
 @Injectable()
 export class ProductService {
+    constructor(private _http: HttpClient){}
     getProducts(): IProduct[] {
         return[
             {
