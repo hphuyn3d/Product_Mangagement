55275523490ce7acbced53f75974d0a7cf4429e4
hhuynh <hhuynh@connellypartners.com>
2018-05-14 23:38:25 -0400

    Imported some debugging library's, created a private method to handle errors add a Do to get a peak of what's being added and a catch to throw the error.

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { IProduct } from './product';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class ProductService {
     private _productUrl = './api/products/products.json';
     constructor(private _http: HttpClient){}
     getProducts(): Observable<IProduct[]> {
-        return this._http.get<IProduct[]>(this._productUrl);
+        return this._http.get<IProduct[]>(this._productUrl)
+            .do(data => console.log('All: ' +JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+    // tslint:disable-next-line:no-trailing-whitespace
+    
+    private handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
     }
 }
+
